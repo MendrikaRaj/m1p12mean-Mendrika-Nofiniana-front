2f5415d32beed141b0f6ca7dc1baef6301387a52
Mendrika <99309350+MendrikaRaj@users.noreply.github.com>
2025-09-15 08:29:08 +0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -33,7 +33,8 @@ export const routes: Routes = [
       },
       {
         path: 'create-service/:appointment_id',
-        component: CreateServiceComponent, // Pass a custom value
+        component: CreateServiceComponent, // Pass a custom value\
+        data: { renderMode: 'default' } // or 'no-prerender'
       },
       {
         path: 'list-service-manager',
